96f7bead1c27641fccda4f14c88a0a9de5378227
Elif <elifintizam@gmail.com>
2023-08-05 17:25:11 +0300

    Model, css, js

--- src/main/java/com/elifintizam/controller/ThymeleafController.java
+++ src/main/java/com/elifintizam/controller/ThymeleafController.java
@@ -1,6 +1,7 @@
 package com.elifintizam.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -11,5 +12,23 @@ public class ThymeleafController {
     //@ResponseBody
     public String getThymeleaf1(){
         return "thymeleaf1";
+    }
+
+    // http://localhost:8080/thymeleaf2
+    @GetMapping("/thymeleaf2")
+    //@ResponseBody
+    public String getThymeleaf2Model(Model model){
+        model.addAttribute("key_model1","I came from Model1");
+        model.addAttribute("key_model2","I came from Model2");
+        return "thymeleaf1";
+    }
+
+    // http://localhost:8080/thymeleaf3
+    @GetMapping("/thymeleaf3")
+    //@ResponseBody
+    public String getThymeleaf3Model(Model model){
+        model.addAttribute("key_model1","I came from Model1");
+        model.addAttribute("key_model2","I came from Model2");
+        return "thymeleaf_file/thymeleaf3";
     }
 }
