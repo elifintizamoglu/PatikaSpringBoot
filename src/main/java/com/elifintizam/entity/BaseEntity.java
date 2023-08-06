72cd9394d31859f0923fe46bd1f0b7599cfa1ab5
Elif <elifintizam@gmail.com>
2023-08-06 16:22:47 +0300

    Entity, Dto, Bean Structures

---
+++ src/main/java/com/elifintizam/entity/BaseEntity.java
@@ -0,0 +1,46 @@
+package com.elifintizam.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.util.Date;
+
+@MappedSuperclass
+@Getter
+@Setter
+@EntityListeners(AuditingEntityListener.class)
+public class BaseEntity {
+
+    @Id
+    @Column(name = "id",nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "create_by")
+    @CreatedBy
+    private String createBy;
+
+    @Column(name = "create_date")
+    @CreatedDate
+    private Date createDate;
+
+    @Column(name = "update_by")
+    @LastModifiedBy
+    private String updateBy;
+
+    @Column(name = "update_date")
+    @LastModifiedDate
+    private Date updateDate;
+
+    @Column(name = "system_auto_date")
+    @Temporal(TemporalType.TIMESTAMP)
+    @CreationTimestamp
+    private Date date;
+}
