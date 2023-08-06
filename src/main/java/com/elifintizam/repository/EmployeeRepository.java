8fafd49d7731b3f33a3dc3b58716e5de3eeb539e
Elif <elifintizam@gmail.com>
2023-08-06 16:50:49 +0300

    Repository (jpa hibernate)

---
+++ src/main/java/com/elifintizam/repository/EmployeeRepository.java
@@ -0,0 +1,12 @@
+package com.elifintizam.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+// hibernate yapısını sağlayacak kod işlemleri kullanacağımız yapılar
+// jparepository > crudrepository daha geniştir
+// repository yapısı interfacedir
+
+@Repository
+public interface EmployeeRepository extends CrudRepository {
+}
